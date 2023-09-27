930e02106d55e81b78851a0561ae9f1225173f77
Nikita <wotblitz362@mail.ru>
2023-09-27 23:21:53 +0300

    chat

--- client/connection/src/webSock.js
+++ client/connection/src/webSock.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+/*import React, {useEffect, useRef, useState} from 'react';
 import axios from "axios";
 
 const WebSock = () => {
@@ -80,6 +80,127 @@ const WebSock = () => {
                             }
                         </div>
                     )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WebSock; */
+
+
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
+const WebSock = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+    const socket = useRef();
+    const [connected, setConnected] = useState(false);
+    const [username, setUsername] = useState('');
+    const [recipient, setRecipient] = useState('');
+    const [userList, setUserList] = useState([]);
+
+    function connect() {
+        socket.current = new WebSocket('ws://localhost:5000');
+
+        socket.current.onopen = () => {
+            setConnected(true);
+            const message = {
+                event: 'connection',
+                username,
+                id: Date.now(),
+            };
+            socket.current.send(JSON.stringify(message));
+        };
+
+        socket.current.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+
+            switch (message.event) {
+                case 'privateMessage':
+                    setMessages(prev => [...prev, message]);
+                    break;
+                case 'userList':
+                    setUserList(message.userList);
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        socket.current.onclose = () => {
+            console.log('Socket закрыт');
+        };
+
+        socket.current.onerror = () => {
+            console.log('Socket произошла ошибка');
+        };
+    }
+
+    const sendMessage = async () => {
+        if (recipient && value) {
+            const message = {
+                username,
+                to: recipient,
+                text: value,
+                id: Date.now(),
+                event: 'privateMessage',
+            };
+            socket.current.send(JSON.stringify(message));
+            setValue('');
+        }
+    };
+
+    if (!connected) {
+        return (
+            <div className="center">
+                <div className="form">
+                    <input
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        type="text"
+                        placeholder="Введите ваше имя"
+                    />
+                    <button onClick={connect}>Войти</button>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="center">
+            <div>
+                <div className="form">
+                    <input
+                        value={recipient}
+                        onChange={(e) => setRecipient(e.target.value)}
+                        type="text"
+                        placeholder="Введите имя получателя"
+                    />
+                    <input
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        type="text"
+                    />
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map((mess) => (
+                        <div key={mess.id}>
+                            <div className="message">
+                                {mess.from}: {mess.text}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="user-list">
+                    <h3>Online Users:</h3>
+                    <ul>
+                        {userList.map((user) => (
+                            <li key={user}>{user}</li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         </div>
--- server/websockets.js
+++ server/websockets.js
@@ -1,4 +1,4 @@
-const ws = require('ws');
+/*const ws = require('ws');
 
 const wss = new ws.Server({
     port: 5000,
@@ -21,6 +21,72 @@ wss.on('connection', function connection(ws) {
 
 function broadcastMessage(message, id) {
     wss.clients.forEach(client => {
+        console.log(id, message)
         client.send(JSON.stringify(message))
     })
+}*/
+
+
+const ws = require('ws');
+
+const wss = new ws.Server({
+    port: 5000,
+}, () => console.log(`Server started on 5000`))
+
+// Store connected clients
+const clients = new Map();
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function (message) {
+        message = JSON.parse(message)
+        switch (message.event) {
+            case 'connection':
+                handleConnection(message, ws);
+                break;
+            case 'privateMessage':
+                handlePrivateMessage(message);
+                break;
+        }
+    });
+
+    ws.on('close', () => {
+        // Remove the client from the clients map
+        clients.delete(ws.username);
+        broadcastUserList();
+    });
+});
+
+function handleConnection(message, ws) {
+    const { username } = message;
+    ws.username = username;
+    clients.set(username, ws);
+
+    broadcastUserList();
+}
+
+function handlePrivateMessage(message) {
+    const { to, text } = message;
+    const from = message.username;
+
+    if (clients.has(to)) {
+        const targetClient = clients.get(to);
+        targetClient.send(JSON.stringify({
+            event: 'privateMessage',
+            from,
+            text,
+        }));
+    }
+}
+
+function broadcastUserList() {
+    const userList = Array.from(clients.keys());
+    const userListMessage = JSON.stringify({
+        event: 'userList',
+        userList,
+    });
+
+    // Broadcast the updated user list to all connected clients
+    clients.forEach(client => {
+        client.send(userListMessage);
+    });
 }
